0ed1e0201793cb42f3725e59e5d133ae7a379837
Aayush Garg <masteraayushgarg@gmail.com>
2024-07-12 00:30:58 +0530

    Added deployed backend link

--- src/models/common.model.ts
+++ src/models/common.model.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.237.170:9000/api/v1'
-
+// export const BASE_URL = 'http://<your-ipv4config>:9000/api/v1'
+export const BASE_URL = 'https://social-media-app-backend-jk8l.onrender.com/api/v1'
 export const Gender = {
     'male' : 'MALE',
     'female' : 'FEMALE'
